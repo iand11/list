5c2b8776db7c0d6b2c525adfc0836dba76fcaeab
iand11 <ian.driscoll.o@gmail.com>
2017-05-26 14:07:52 -0700

    Erase twilio keys

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -15,18 +15,5 @@ post '/receive_sms' do
   
 end 
 
-post '/send_sms' do 
-  to = params["to"]
-  message = params["body"]
 
-  client = Twilio::REST::Client.new(
-    "AC36059c5195328933be6d8f21067c4c6b",
-    "0bf1309e2789d7d967bfbb8b55a96d1d"
-    )
-  client.message.create(
-    to: to,
-    from: "+15162102506",
-    body: message
-    )
-end 
 
