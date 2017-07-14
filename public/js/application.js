eff39216376cc3d920a780ae6fba260c5cc5d3e1
iand11 <ian.driscoll.o@gmail.com>
2017-07-14 15:22:22 -0700

    Clean up code in both index.html and application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$(".container").fadeIn("slow");
-	$(".edit_form").hide();
-	$(".header_login_form").hide();
-	$(".header_register_form").hide();
-	$(".new_entry_form").hide();
+	$('.container').fadeIn('slow');
+	$('.edit_form').hide();
+	$('.header_login_form').hide();
+	$('.header_register_form').hide();
+	$('.new_entry_form').hide();
 	newEntryButtonListener();
 	newEntryFormListener();
 	loginButtonListener();
@@ -12,84 +12,78 @@ $(document).ready(function() {
 	entryRowListener();
 });
 
-var newEntryButtonListener = function(){
-	$(".new_entry_button").on("click", function(event){
+var newEntryButtonListener = function() {
+	$('.new_entry_button').on('click', function(event) {
 		event.preventDefault();
-		$(".new_entry_form").show();
+		$('.new_entry_form').show();
 		$(this).hide();
-	})
-}
+	});
+};
 
-var newEntryFormListener = function(){
-	$(".new_entry_form").on("submit", ".full_entry_form", function(event){
+var newEntryFormListener = function() {
+	$('.new_entry_form').on('submit', '.full_entry_form', function(event) {
 		event.preventDefault();
 
-		$form = $(this)
-		url = $form.attr("action")
-		method = $form.attr("method")
+		$form = $(this);
+		url = $form.attr('action');
+		method = $form.attr('method');
 		data = $form.serialize();
 
 		$.ajax({
 			url: url,
 			method: method,
 			data: data
-		})
+		}).done(function(responce) {
+			$('.items_list').append(responce);
+			$('.full_entry_form')[0].reset;
+			$('.full_entry_form').hide();
+			$('.new_entry_button').show();
+			$('.edit_form').hide();
+		});
+	});
+};
 
-		.done(function(responce){
-			$(".items_list").append(responce);
-			$(".full_entry_form")[0].reset
-			$(".full_entry_form").hide();
-			$(".new_entry_button").show();
-			$(".edit_form").hide();
-		})
-	})
-}
-
-var loginButtonListener = function(){
-	$(".login_button").on("click", function(event){
+var loginButtonListener = function() {
+	$('.login_button').on('click', function(event) {
 		event.preventDefault();
-		$(".header_login_form").show();
+		$('.header_login_form').show();
 		$(this).hide();
-	})
-}
+	});
+};
 
-var registerButtonListener = function(){
-	$(".register_button").on("click", function(event){
+var registerButtonListener = function() {
+	$('.register_button').on('click', function(event) {
 		event.preventDefault();
-		$(".header_register_form").show();
+		$('.header_register_form').show();
 		$(this).hide();
-		$(".login_button").hide();
-	})
-}
+		$('.login_button').hide();
+	});
+};
 
-var editButtonListener = function(){
-	$(".items_list").on("submit",".edit_form",function(event){
+var editButtonListener = function() {
+	$('.items_list').on('submit', '.edit_form', function(event) {
 		event.preventDefault();
 
-		$form = $(this)
-		url = $form.attr("action")
-		method = $form.attr("method")
+		$form = $(this);
+		url = $form.attr('action');
+		method = $form.attr('method');
 		data = $form.serialize();
 
 		$.ajax({
 			url: url,
 			method: method,
 			data: data
-		})
+		}).done(function(responce) {
+			$('.items_list').replaceWith(responce);
+		});
+	});
+};
 
-		.done(function(responce){
-			$(".items_list").replaceWith(responce)
-		})
-	})
-}
-
-var entryRowListener = function(){
-	$(".items_list").on("click",".entry_row", function(){
-		$(".edit_form").slideDown("slow");
-	})	
-	$(".items_list").on("dblclick",".entry_row", function(){
-		$(".edit_form").slideUp("slow");
-	})
-
-}
-
+var entryRowListener = function() {
+	$('.items_list').on('click', '.entry_row', function() {
+		$('.edit_form').slideDown('slow');
+	});
+	$('.items_list').on('dblclick', '.entry_row', function() {
+		$('.edit_form').slideUp('slow');
+	});
+};
